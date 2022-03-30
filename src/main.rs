1ecbdb35b11971b5e3b8958e5855275569fbe9ab
TrentConley <trentconley@gmail.com>
2022-03-29 19:00:20 -0500

    learning variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let num: i32;
+    if true {
+        num = 33;
+    } else {
+        num = 1;
+    } // satify the compiler that num will be initilized
+    println!("num is {}", num);
 }
